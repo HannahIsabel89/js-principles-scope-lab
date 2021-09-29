dab74409e09a32933bf0e6eee80dc6262ea7686c
Hannah <hannahwoodward@hotmail.com>
2021-09-29 11:24:55 +0800

    update properly

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your solution in this file!
+var customerName = "Hannah"
+
+function upperCaseCustomerName () {
+    console.log(customerName.toUpperCase());
+}
+
+upperCaseCustomerName ();
