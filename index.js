384d33f35f0cc15565d39350fb7173c37d62f1ef
Hannah <hannahwoodward@hotmail.com>
2021-09-30 18:18:09 +0800

    not able to test correctly

--- index.js
+++ index.js
@@ -2,7 +2,24 @@
 var customerName = "Hannah"
 
 function upperCaseCustomerName () {
-    console.log(customerName.toUpperCase());
+    customerName = customerName.toUpperCase();
 }
 
 upperCaseCustomerName ();
+
+
+function setBestCustomer () {
+    bestCustomer = "not Bob"
+}
+
+function overwriteBestCustomer () {
+    bestCustomer = 'maybe Bob'
+}
+
+const leastFavoriteCustomer = "Sally"
+
+function changeLeastFavoriteCustomer () {
+    leastFavoriteCustomer = "Fran"
+}
+
+changeLeastFavoriteCustomer();
